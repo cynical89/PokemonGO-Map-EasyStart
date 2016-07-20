c1a40f93a011f22cd59185da533e46ec7bb9d942
Anthony Michelizzi <cynical89@gmail.com>
2016-07-19 18:40:03 -0700

    minor changes
    Added some minor error checking.
    
    [x]- cant open another server while one is running
    [x]- you can now stop the server from inside the application
    [x]- if you stop the server outside of the program it check and restarts
    as appropriate
    [x]- added an icon and a window name change

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,43 +22,94 @@ namespace PokeGoMap_EasyButton
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        #region global vars
+        //bool to check that we are running so we can't hit _python again
+        private bool isRunning = false;
+        private string _args;
+
+        //processes that will be started
+        private readonly Process _python = new Process();
+        private readonly Process _web = new Process();
+        #endregion
+
+        #region Main Window
         public MainWindow()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region Run
 
         private void RunButton_Click(object sender, RoutedEventArgs e)
         {
             //auth arg
-            string _auth = "-a " + auth.Text;
+            string auth = "-a " + authService.Text;
             //user arg
-            string _user = "-u " + userText.Text;
+            string user = "-u " + userText.Text;
             //pass arg
-            string _pass = "-p " + passText.Text;
+            string pass = "-p " + passText.Text;
             //location arg
-            string _locate = "-l " + "'" + locateText.Text + "'";
+            string locate = "-l " + "'" + locateText.Text + "'";
             //steps arg
-            string _steps = "-st " + steps1.Text;
+            string steps = "-st " + steps1.Text;
 
             //main arg string
-            string args = "example.py " + _auth + " " + _user + " " + _pass + " " + _locate + " " + _steps;
+            _args = "example.py " + auth + " " + user + " " + pass + " " + locate + " " + steps;
 
-            //execute the example.py
-            run_cmd("C:/Python27/python.exe", args);
-            //open web browser after 5 seconds
-            Task.Delay(5000).ContinueWith(_ =>
+            if (isRunning)
+            {
+                if (_python.HasExited)
                 {
-                    System.Diagnostics.Process.Start("http://localhost:5000");
+                    isRunning = false;
+                    run_cmd("C:/Python27/python.exe", _args);
                 }
-            );
+                else
+                {
+                    MessageBox.Show("The python server is already running. Stop the server and try again.");
+                }
+            }
+            else
+            {
+                //execute the example.py
+                run_cmd("C:/Python27/python.exe", _args);
+                isRunning = true;
+            }
         }
 
+        #endregion
+
+        #region Stop
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isRunning)
+            {
+                //stop processes and let us open everything back up.
+                _python.CloseMainWindow();
+                isRunning = false;
+            }
+        }
+        #endregion
+
+        #region Run Python Server
         private void run_cmd(string cmd, string args)
         {
-            ProcessStartInfo start = new ProcessStartInfo();
-            start.FileName = cmd;//cmd is full path to python.exe
-            start.Arguments = args;//args is path to .py file and any cmd line args
-            Process.Start(start);
+            //cmd is full path to python.exe
+            _python.StartInfo.FileName = cmd;
+            //_args is path to .py file and any cmd line _args
+            _python.StartInfo.Arguments = args;
+            //open python server
+            _python.Start();
+            //we are running now
+            isRunning = true;
+            //open the web
+            Task.Delay(5000).ContinueWith(_ =>
+            {
+                System.Diagnostics.Process.Start("http://localhost:5000");
+            }
+    );
         }
+        #endregion
     }
 }
